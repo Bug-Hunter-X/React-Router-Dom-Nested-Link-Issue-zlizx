41f14b3dc137cc218fed1434707bde516402b912
BugHunter-X <askzenai@gmail.com>
2024-12-17 17:30:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,36 @@
+```javascript
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* ... other routes ... */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link to="/about">Go to About</Link>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      {/* Corrected use of absolute paths */}
+      <Link to="/">Go to Home</Link>
+      {/* This link is removed as it is incorrect */}
+      {/* <Link to="home">Go to Home - Incorrect path</Link> */}
+    </div>
+  );
+}
+```
