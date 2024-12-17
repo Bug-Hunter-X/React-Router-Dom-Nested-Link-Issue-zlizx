9beee530b57c387dfa417291e09b86c4599fca15
BugHunter-X <askzenai@gmail.com>
2024-12-17 17:30:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,35 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* ... other routes ... */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link to="/about">Go to About</Link>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      {/* This is a nested Link that causes the error */}
+      <Link to="/">Go to Home</Link>
+      <Link to="home">Go to Home - Incorrect path</Link>
+    </div>
+  );
+}
+```
